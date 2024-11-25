7a4fb977ccd89ff9964ea2896de0ec5a5b535b20
Ramil Gilmanov <gilmanoffdesign@gmail.com>
2024-11-25 12:56:46 +0300

    Задокументировано и ещё раз протестировано.

--- src/main/java/com/application/techxercise/controllers/UserTaskController.java
+++ src/main/java/com/application/techxercise/controllers/UserTaskController.java
@@ -1,11 +1,13 @@
 package com.application.techXercise.controllers;
 
 import com.application.techXercise.dto.TaskResponseDTO;
-import com.application.techXercise.entity.TaskEntity;
 import com.application.techXercise.exceptions.TaskNotFoundException;
 import com.application.techXercise.services.TaskService;
 import com.application.techXercise.utils.TaskPriority;
 import com.application.techXercise.utils.TaskStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,7 +25,11 @@ public class UserTaskController {
         this.taskService = taskService;
     }
 
-    // Эндпоинт для получения задач исполнителя
+    @Operation(summary = "Получить задачи по исполнителю", description = "Метод возвращает задачи, которые назначены пользователю-исполнителю.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Задачи успешно получены"),
+            @ApiResponse(responseCode = "400", description = "Неверные параметры запроса")
+    })
     @GetMapping("/executor")
     public ResponseEntity<Page<TaskResponseDTO>> getTasksByExecutor(
             @PathVariable Long userId,
@@ -36,8 +42,11 @@ public class UserTaskController {
         return ResponseEntity.ok(tasks);
     }
 
-
-    // Эндпоин для получения задач по автору (хотя автор всегда админ)
+    @Operation(summary = "Получить задачи по автору", description = "Метод возвращает задачи, созданные пользователем-автором.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Задачи успешно получены"),
+            @ApiResponse(responseCode = "400", description = "Неверные параметры запроса")
+    })
     @GetMapping("/author")
     public ResponseEntity<Page<TaskResponseDTO>> getTasksByAuthor(
             @PathVariable Long userId,
@@ -50,16 +59,25 @@ public class UserTaskController {
         return ResponseEntity.ok(tasks);
     }
 
-    // Получить конкретную задачу
+    @Operation(summary = "Получить задачу по ID", description = "Метод возвращает информацию о задаче по её идентификатору.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Задача успешно получена"),
+            @ApiResponse(responseCode = "404", description = "Задача не найдена")
+    })
     @GetMapping("/{taskId}")
     public ResponseEntity<TaskResponseDTO> getTaskById(@PathVariable long taskId) throws TaskNotFoundException {
         TaskResponseDTO taskEntity = taskService.getTaskById(taskId);
         return ResponseEntity.ok(taskEntity);
     }
 
-    // Редактирование объектов
+    @Operation(summary = "Обновить статус задачи", description = "Метод позволяет обновить статус задачи, если пользователь является её исполнителем.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Задача успешно обновлена"),
+            @ApiResponse(responseCode = "404", description = "Задача не найдена")
+    })
     @PatchMapping("/{taskId}/")
-    public ResponseEntity<TaskResponseDTO> updateTaskStatus(@PathVariable long taskId, @Valid @RequestParam TaskStatus status) throws TaskNotFoundException {
+    public ResponseEntity<TaskResponseDTO> updateTaskStatus(@PathVariable long taskId
+            , @Valid @RequestParam TaskStatus status) throws TaskNotFoundException {
         TaskResponseDTO updatedTaskEntity = taskService.updateTaskStatusByUser(taskId, status);
         return updatedTaskEntity != null ?
                 ResponseEntity.ok(updatedTaskEntity) :
